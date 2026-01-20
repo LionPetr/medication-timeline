7c586573e018c8c8a1a0cfd5322a76c49cdece82
lion <lionpetrenko@gmail.com>
2026-01-20 08:47:11 -0800

    added initial setup and created models for data

---
+++ backend/medications/models.py
@@ -0,0 +1,61 @@
+from django.db import models
+
+class Medication(models.Model):
+    name = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.name
+
+class MedicationTimelineEntry(models.Model):
+    """
+    Represents a signle medication course on the main timeline
+    """
+    medication = models.ForeignKey(
+        Medication,
+        on_delete=models.CASCADE,
+        related_name="timeline_entries"
+    )
+    start_date = models.DateField(null=True, blank=True)
+    end_date = models.DateField(null=True, blank=True)
+    current_dose = models.CharField(max_length=100, blank=True)
+    current_route = models.CharField(max_length=20, blank=True)
+    source_facility = models.CharField(max_length=200, blank=True)
+
+    notes = models.TextField(blank=True) #general notes
+    conflict_notes = models.TextField(blank=True)
+    conflicting = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.medication.name} ({self.start_date or 'Unknown'} - {self.end_date or 'Present'})"
+
+#change within a medication course
+class MedicationHistory(models.Model):
+    ROUTE_CHOICES = [
+        ("oral", "Oral"),
+        ("iv", "IV"),
+        ("subcutaneous", "Subcutaneous"),
+        ("topical", "Topical"),
+        ("other", "Other"),
+    ]
+
+    timeline_entry = models.ForeignKey(
+        MedicationTimelineEntry,
+        on_delete=models.CASCADE,
+        related_name="history"
+    )
+    dose = models.CharField(max_length=100)
+    route = models.CharField(max_length=20, choices=ROUTE_CHOICES)
+    end_date = models.DateField(null=True, blank=True)
+    source_facility = models.CharField(max_length=200, blank=True)
+
+    change_notes = models.TextField(blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["created_at"]
+
+    def __str__(self):
+        return f"{self.timeline_entry.medication.name} ({self.dose}, {self.route}, {self.created_at.date()})"
+    
